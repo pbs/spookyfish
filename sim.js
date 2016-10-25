a56368f843ba6206fe67ca330db447686ed1e637
Chip Bell <cbell@pbs.org>
2016-10-25 09:51:56 -0400

    Removing my boid code in place of improvements

--- sim.js
+++ sim.js
@@ -1,3 +1,5 @@
+var boids = require('boids');
+
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 
@@ -5,19 +7,30 @@ var SIZE = 500;
 canvas.setAttribute('width', SIZE);
 canvas.setAttribute('width', SIZE);
 
-MAX_Y = SIZE;
-var flock = new Flock(20, 0, SIZE);
-var animate = function() {
-  var dt = 1 / 60;
+var flock = boids({
+  boids: 200,              // The amount of boids to use 
+  speedLimit: 2,          // Max steps to take per tick 
+  accelerationLimit: 0.1,   // Max acceleration per tick 
+  separationDistance: 60, // Radius at which boids avoid others 
+  alignmentDistance: 180, // Radius at which boids align with others 
+  choesionDistance: 180,  // Radius at which boids approach others 
+  separationForce: 0.15,  // Speed to avoid at 
+  alignmentForce: 0.25,   // Speed to align with other boids 
+  choesionForce: 0.1,     // Speed to move towards other boids 
+  attractors: [
+    [SIZE / 2, SIZE / 2]
+  ]
+});
 
-  flock.update(dt);
+var animate = function() {
+  flock.tick();
 
   // re-render
   ctx.clearRect(0, 0, SIZE, SIZE);
   ctx.fillStyle = 'black';
-  for(var i = 0; i < flock.boids.length; i++) {
-    ctx.fillRect(flock.boids[i].x, flock.boids[i].y, 2, 2);
-  }
+  flock.boids.forEach(function(boid) {
+    ctx.fillRect(boid[0], boid[1], 2, 2);
+  });
 
   requestAnimationFrame(animate);
 }

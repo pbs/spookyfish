39b55546be2908eb911341d56f6f36aeb76223d9
Chip Bell <cbell@pbs.org>
2016-10-25 16:35:06 -0400

    Tidying a little

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var faye = require('faye');
 
-var app = express();
-
-app.use(express.static('public'));
-
 console.log('Starting express app...');
+var app = express();
+app.use(express.static('public'));
 app.listen(8080);
 
 console.log('Starting up faye...');
@@ -13,7 +11,6 @@ var pubsub = new faye.NodeAdapter({
   mount: '/faye',
   timeout: 45,
 });
-
 pubsub.attach(app);
 
 console.log('And we\'re ready!');

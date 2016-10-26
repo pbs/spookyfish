873f4df9712b5e4e1eb71ce208c68adb8dd782d7
Chip Bell <cbell@pbs.org>
2016-10-26 16:32:22 -0400

    Rudamentary client sync (teleportation)

--- client/flock-sync.js
+++ client/flock-sync.js
@@ -1,20 +1,18 @@
-var boids = require('boids');
 var viewport = require('./viewport');
 var deadReckoning = require('./dead-reckoning');
 var messages = require('./messages');
-
-var flock = null;
+var flock = require('../shared/flock');
 
 module.exports = {
   init: function() {
-    messages.subscribe(function() {
-      console.log('Received a message!');
-    });
+    messages.subscribe(function(data) {
+      this.receiveVisibleFlockUpdate(data.boids);
+    }.bind(this));
   },
 
   receiveVisibleFlockUpdate: function(newBoids) {
     for(var i = 0; i < newBoids.length; i++) {
-      flock.boids[i] = deadReckoning.zerothOrder(flock.boids[i], newBoids[i]);
+      flock.boids()[i] = deadReckoning.zerothOrder(flock.boids()[i], newBoids[i]);
     }
   }
 };

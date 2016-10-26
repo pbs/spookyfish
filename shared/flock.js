d8f3b61752888cde9194f617bc4a848fda5e9409
Chip Bell <cbell@pbs.org>
2016-10-26 19:44:44 -0400

    Tweaking some params

--- shared/flock.js
+++ shared/flock.js
@@ -6,14 +6,14 @@ module.exports = {
   init: function(width, height) {
     flock = boids({
       boids: 100,              // The amount of boids to use 
-      speedLimit: 2,          // Max steps to take per tick 
+      speedLimit: 1,          // Max steps to take per tick 
       accelerationLimit: 0.2,   // Max acceleration per tick 
       separationDistance: 60, // Radius at which boids avoid others 
-      alignmentDistance: 180, // Radius at which boids align with others 
+      alignmentDistance: 500, // Radius at which boids align with others 
       cohesionDistance: 180,  // Radius at which boids approach others 
       separationForce: 0.05,  // Speed to avoid at 
-      alignmentForce: 0.25,   // Speed to align with other boids 
-      choesionForce: 0.5,     // Speed to move towards other boids 
+      alignmentForce: 0.50,   // Speed to align with other boids 
+      choesionForce: 0.05,     // Speed to move towards other boids 
     });
 
     flock.boids.forEach(function(boid) {

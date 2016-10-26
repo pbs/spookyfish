32ed757ff875a0082470687d3df6d61d5f856fa4
Chip Bell <cbell@pbs.org>
2016-10-26 19:33:31 -0400

    Adding second order correct. Meh

--- client/flock-sync.js
+++ client/flock-sync.js
@@ -15,9 +15,14 @@ module.exports = {
     for(var i = 0; i < newBoids.length; i++) {
       var dx = flock.boids()[i][0] - newBoids[i][0];
       var dy = flock.boids()[i][1] - newBoids[i][1];
-      totalDistanceError += Math.sqrt(dx * dx + dy * dy);
+      var distanceError = Math.sqrt(dx * dx + dy * dy);
+      totalDistanceError += distanceError;
 
-      flock.boids()[i] = deadReckoning.firstOrder(flock.boids()[i], newBoids[i]);
+      if(distanceError > 20) {
+        flock.boids()[i] = deadReckoning.zerothOrder(flock.boids()[i], newBoids[i]);
+      } else {
+        flock.boids()[i] = deadReckoning.secondOrder(flock.boids()[i], newBoids[i]);
+      }
     }
     console.log('Average position error:', totalDistanceError / newBoids.length);
   }

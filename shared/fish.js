e80dad42d76de1d8eb954d813c9e3edf1536a03a
Chip Bell <cbell@pbs.org>
2016-10-26 22:25:43 -0400

    rename

--- shared/fish.js
+++ shared/fish.js
@@ -1,15 +1,15 @@
-var FLOCK_MIN_X = 0;
-var FLOCK_MAX_X = 500;
-var FLOCK_MIN_Y = 0;
-var FLOCK_MAX_Y = 500;
+var SCHOOL_MIN_X = 0;
+var SCHOOL_MAX_X = 500;
+var SCHOOL_MIN_Y = 0;
+var SCHOOL_MAX_Y = 500;
 
 var rand = function(a, b) {
   return Math.random() * (b - a) + a;
 }
 
 var Fish = function(options) {
-  this.x = rand(FLOCK_MIN_X, FLOCK_MAX_X);
-  this.y = rand(FLOCK_MIN_Y, FLOCK_MAX_Y);
+  this.x = rand(SCHOOL_MIN_X, SCHOOL_MAX_X);
+  this.y = rand(SCHOOL_MIN_Y, SCHOOL_MAX_Y);
   this.vx = 1;
   this.vy = 0;
 };
@@ -24,19 +24,19 @@ Fish.prototype.update = function() {
   this.vy += Math.random() * dt;
 
   // Wall collision
-  if(this.x < FLOCK_MIN_X) {
-    this.x = FLOCK_MIN_X;
+  if(this.x < SCHOOL_MIN_X) {
+    this.x = SCHOOL_MIN_X;
     this.vx = Math.abs(this.vx);
-  } else if(this.x > FLOCK_MAX_X) {
-    this.x = FLOCK_MAX_X;
+  } else if(this.x > SCHOOL_MAX_X) {
+    this.x = SCHOOL_MAX_X;
     this.vx = -Math.abs(this.vx);
   }
   
-  if(this.y < FLOCK_MIN_Y) {
-    this.y = FLOCK_MIN_Y;
+  if(this.y < SCHOOL_MIN_Y) {
+    this.y = SCHOOL_MIN_Y;
     this.vy = Math.abs(this.vy);
-  } else if(this.y > FLOCK_MAX_Y) {
-    this.y = FLOCK_MAX_Y;
+  } else if(this.y > SCHOOL_MAX_Y) {
+    this.y = SCHOOL_MAX_Y;
     this.vy = -Math.abs(this.vy);
   }
 }

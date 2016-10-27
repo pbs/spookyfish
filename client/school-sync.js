09b4981749c0cc01c6fc112b2b3c808d63ada622
Chip Bell <cbell@pbs.org>
2016-10-26 22:41:26 -0400

    Adding back initial syncing

--- client/school-sync.js
+++ client/school-sync.js
@@ -19,11 +19,11 @@ module.exports = {
   receiveVisibleSchoolUpdate: function(newFish) {
     var totalDistanceError = 0;
     for(var i = 0; i < newFish.length; i++) {
-      //if(neverSynced) {
-      //  deadReckoning.zerothOrder(school.get(i), newFish[i]);
-      //} else {
+      if(neverSynced) {
+        deadReckoning.zerothOrder(school.get(i), newFish[i]);
+      } else {
       //  deadReckoning.firstOrder(school.get(i), newFish[i]);
-      //}
+      }
     }
 
     neverSynced = false;

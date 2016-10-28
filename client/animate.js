8b5fc7e3924a14c0fb02ba64551d901615e05775
Chip Bell <cbell@pbs.org>
2016-10-28 15:32:34 -0400

    Adding some seaweed

--- client/animate.js
+++ client/animate.js
@@ -15,9 +15,17 @@ var fishImages = [
   "src-img/halloween_fish3.png"
 ];
 
+var seaweed = [
+  "src-img/seaweed-1.png",
+  "src-img/seaweed-2.png",
+  "src-img/seaweed-3.png",
+  "src-img/seaweed-4.png",
+  "src-img/seaweed-5.png"
+];
+
 module.exports = {
   load: function (callback){
-    var allImages = fishImages.concat(['src-img/fish_base.png']);
+    var allImages = fishImages.concat(seaweed).concat(['src-img/fish_base.png']);
     PIXI.loader.add(allImages).load(function() {
       this.init();
       callback && callback();
@@ -76,6 +84,17 @@ module.exports = {
     castle.x = 0;
     castle.y = window.innerHeight;
     stage.addChild(castle);
+
+    var seaweedSprite;
+    for(var x = Math.random() * 100; x < window.innerWidth; x += Math.random() * 200 + 400) {
+      var index = Math.floor(Math.random() * seaweed.length);
+      seaweedSprite = new PIXI.Sprite(PIXI.loader.resources[seaweed[index]].texture);
+      seaweedSprite.scale.x = seaweedSprite.scale.y = 0.2;
+      seaweedSprite.anchor.y = 1.0;
+      seaweedSprite.x = x;
+      seaweedSprite.y = window.innerHeight;
+      stage.addChild(seaweedSprite);
+    }
 
     renderer.render(stage);
     requestAnimationFrame(this.update.bind(this));

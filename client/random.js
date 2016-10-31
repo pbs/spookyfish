8dddcac0b1d207bb3e39bf667e72e470def1f69c
Chip Bell <cbell@pbs.org>
2016-10-31 09:55:04 -0400

    Adding a seeded rng

--- client/random.js
+++ client/random.js
@@ -1,16 +1,24 @@
+var seedrandom = require('seedrandom');
+
+var rng = seedrandom(0);
+
 module.exports = {
+  seed: function(x) {
+    rng = seedrandom(x);
+  },
+
   // Gets a random number between a and b
   between: function(a, b) {
-    return Math.random() * (b - a) + a;
+    return rng.quick() * (b - a) + a;
   },
 
   // Return true with probability p
   maybe: function(p) {
-    return Math.random() <= p;
+    return rng.quick() <= p;
   },
 
   // gets a random thing from an array
   fromArray: function(array) {
-    return array[Math.floor(Math.random() * array.length)];
+    return array[Math.floor(rng.quick() * array.length)];
   }
 };
